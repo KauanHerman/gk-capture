50620e4db1dcce5d0e226d2bc01f07e7326246c5
KauanHerman <143849582+KauanHerman@users.noreply.github.com>
2026-08-07 15:54:27 -0300

    Primeira versão do site GK Capture

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+const header = document.querySelector(".header");
+
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 100) {
+
+        header.style.position = "fixed";
+
+        header.style.background =
+            "rgba(5, 5, 5, 0.92)";
+
+        header.style.backdropFilter =
+            "blur(12px)";
+
+        header.style.webkitBackdropFilter =
+            "blur(12px)";
+
+    } else {
+
+        header.style.position = "absolute";
+
+        header.style.background =
+            "transparent";
+
+        header.style.backdropFilter =
+            "none";
+
+        header.style.webkitBackdropFilter =
+            "none";
+
+    }
+
+});
